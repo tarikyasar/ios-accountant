ca5cfce0ce0d9a41596408917c3e435f7c302dce
Tarik Yasar <m.tarikyasar@gmail.com>
2025-11-03 21:01:28 +0300

    Initial Commit

---
+++ Accountant/AccountantApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccountantApp.swift
+//  Accountant
+//
+//  Created by Tarik Yasar on 3.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct AccountantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
